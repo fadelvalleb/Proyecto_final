21ca6453601000c7cc429924fd770fc87971edd7
fadelvalleb <54915646+fadelvalleb@users.noreply.github.com>
2019-10-01 22:11:00 -0300

    Definición de los contenidos de la presentación

--- Scripts.R
+++ Scripts.R
@@ -1,4 +1,5 @@
-###se cargan los paquetes
+###se cargan los paquetes correspondientes al análisis
 library(tidyverse)
 library(ggplot2)
+library(kableExtra)
 ## SE van a ordenar los datos por género en primer lugar.
